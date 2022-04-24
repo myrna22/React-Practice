b3da6fa19f8b16ba56d7b36628634dfba4722d55
myrna22 <mirnazayada80@yahoo.com>
2022-04-24 09:42:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import User from './component/User';
+import UniqueId from 'react-html-id'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    UniqueId.enableUniqueIds(this);
+    this.state = {
+      users: [
+        {id:this.nextUniqueId(), name: 'username', age:22},
+        {id:this.nextUniqueId(), name: 'username', age:34},
+        {id:this.nextUniqueId(), name: 'username', age:28},
+      ]
+    }
+  }
+
+  deleteUser = (index, e) => {
+    const users = Object.assign([], this.state.users);
+    users.splice(index, 1);
+    this.setState({users:users})
+  }
+
+  changeUserName = (id, e) => {
+      const index = this.state.users.findIndex ((user)=>{
+        return user.id === id
+      });
+      const user = Object.assign({}, this.state.users[index]);
+      user.name = e.target.value;
+      const users = Object.assign([], this.state.users);
+      users[index] = user;
+      this.setState({users:users});
+  }
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {
+            this.state.users.map((user, index) => {
+              return(<User 
+                age={user.age} 
+                key={user.id}
+                delEvent = {this.deleteUser.bind(this, index)}
+                changeEvent={this.changeUserName.bind(this, user.id)}
+                
+                >{user.name}</User>)
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
 }
-
-export default App;
